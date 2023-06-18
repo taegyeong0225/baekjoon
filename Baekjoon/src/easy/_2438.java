b308933d733500428b656c37586622f8f01b7482
Taegyeong Go <202244042@itc.ac.kr>
2023-06-19 02:19:36 +0900

    first file-23.06.19

---
+++ Baekjoon/src/easy/_2438.java
@@ -0,0 +1,19 @@
+package easy;
+
+import java.util.Scanner;
+
+public class _2438 {
+	public static void main(String[] args) {
+		// 별 찍기
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		sc.close();
+		
+		for(int i = 0; i < input; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print("*");
+				}
+			System.out.println();
+		}
+	}
+}
