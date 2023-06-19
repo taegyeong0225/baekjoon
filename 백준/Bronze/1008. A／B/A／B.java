99e92b3e440a8c7987aad5f2430eb9628fb5d546
taegyeong0225 <108190809+taegyeong0225@users.noreply.github.com>
2023-06-19 20:36:39 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        double a = sc.nextInt();
+	        double b = sc.nextInt();
+	        double result = a / b;
+	        System.out.print(result);
+	        sc.close();
+	    }
+	}
