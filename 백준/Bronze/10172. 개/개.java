5d0609b9eb49b4996aab9a1df18e63c93400d516
taegyeong0225 <108190809+taegyeong0225@users.noreply.github.com>
2023-06-19 22:39:24 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		String a = "|\\_/|";
+		String b = "|q p|   /}";
+		String c = "( 0 )\"\"\"\\ ";
+		String d = "|\"^\"`    |";
+	    String e = "||_/=\\\\__|";
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	    System.out.println(e);
+	}
+}
