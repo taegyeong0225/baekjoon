8637d172c57320a935d8c2da46db60dc525b8c02
taegyeong0225 <108190809+taegyeong0225@users.noreply.github.com>
2023-06-25 22:00:01 +0900

    [Bronze V] Title: A+B - 8, Time: 180 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -3,22 +3,13 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int caseNum = sc.nextInt();
 		
-		int a = 0;
-		int b = 0;
-		int[][] arr = new int[caseNum][3];
-		for(int i = 0; i < caseNum; i++) {
-			a = sc.nextInt();
-			b = sc.nextInt();
-			int c = a + b;
-			arr[i][0] += a;
-			arr[i][1] += b;
-			arr[i][2] += c;
+		int num = sc.nextInt();
+		for(int i = 1; i <= num; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.printf("Case #%d: %d + %d = %d\n", i, a, b, a+b);
 		}
-		sc.close();
-		
-		for(int j = 1; j < caseNum + 1; j++)
-			System.out.printf("Case #%d: %d + %d = %d\n", j, arr[j-1][0], arr[j-1][1], arr[j-1][2]);
+        sc.close();
 	}
 }
