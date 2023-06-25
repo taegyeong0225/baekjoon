32da951a566dcaf6f95093144403aeaf7efe8763
taegyeong0225 <108190809+taegyeong0225@users.noreply.github.com>
2023-06-25 22:26:33 +0900

    [Bronze V] Title: A+B - 5, Time: 128 ms, Memory: 13100 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// a와 b가 0일 때까지 a와 b 입력받고 더한 값 저장하기
+
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		int a = 1; int b = 1;
+		while(!(a == 0 && b == 0)) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			int c = a + b;
+			arr.add(c);
+		}
+		for(int i = 0; i < arr.size() - 1; i++)
+			System.out.println(arr.get(i));
+			
+		sc.close();
+	}
+}
