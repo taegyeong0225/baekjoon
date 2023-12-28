7f52ce888af9d7a5a417048a3cc977d1f3876df8
Taegyeong Go <202244042@itc.ac.kr>
2023-12-28 16:47:15 +0900

    10951. A+B - 4 성공

--- src/repeat/_10951.java
+++ src/repeat/_10951.java
@@ -4,8 +4,31 @@ import java.util.Scanner;
 
 public class _10951 {
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
+		while(sc.hasNextLine()) {
+			String num = sc.nextLine();
+			
+			if (num.isEmpty()) {
+                break;
+            }
+			
+			String[] arguNum = num.split(" ");
+			
+			String one;
+			String two;
+			
+			if(arguNum.length >= 2) {
+				one = arguNum[0];
+				two = arguNum[1];
+			
+				int firstNum = Integer.parseInt(one);
+				int secondNum = Integer.parseInt(two);
+					
+				System.out.println(firstNum + secondNum);
+			}
+		}
 		sc.close();
 	}
 }
