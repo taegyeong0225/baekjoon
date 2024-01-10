3a141e7beef71e5c999da9f60ae716a2322e6ba5
taegyeong go <202244042@itc.ac.kr>
2024-01-11 08:35:14 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 1184 ms, Memory: 80244 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -1,10 +1,10 @@
 import sys
 
-input = sys.stdin.readline
+N = int(sys.stdin.readline())
 
 task, result = [], 0
-for _ in range(int(input())):
-    new_task = list(map(int, input().split()))
+for _ in range(N):
+    new_task = list(map(int, sys.stdin.readline().split()))
     if new_task[0] == 1:
         task.append((new_task[1], new_task[2]))
 
